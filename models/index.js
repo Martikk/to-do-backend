683a3662c8d80192ec0719ec2f58ab03bc812152
your_name <alex.martishyn@gmail.com>
2024-05-16 21:33:45 -0700

    Enable CORS

--- models/index.js
+++ models/index.js
@@ -1,31 +1,63 @@
-const { Sequelize } = require('sequelize');
-const config = require('../config/config');
-const env = process.env.NODE_ENV || 'development';
-const sequelizeConfig = config[env];
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./models');
 
-const sequelize = new Sequelize(sequelizeConfig.database, sequelizeConfig.username, sequelizeConfig.password, {
-  host: sequelizeConfig.host,
-  dialect: sequelizeConfig.dialect,
-  port: sequelizeConfig.port,
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());  // Enable CORS for all routes
+app.use(bodyParser.json());
+
+// Sync database
+db.sequelize.sync().then(() => {
+  console.log(`Server running on http://localhost:${port}`);
 });
 
-// Test the connection
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-db.Todo = require('./todo')(sequelize, Sequelize);
-
-// Sync database with alter: true
-db.sequelize.sync({ alter: true }).then(() => {
-  console.log('Database synced');
+// Routes
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await db.Todo.findAll();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).send("Error fetching todos");
+  }
 });
 
-module.exports = db;
+app.post('/todos', async (req, res) => {
+  try {
+    const newTodo = await db.Todo.create(req.body);
+    res.json(newTodo);
+  } catch (error) {
+    res.status(500).send("Error creating todo");
+  }
+});
+
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const updatedTodo = await db.Todo.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+    });
+    res.json(updatedTodo[1][0]);
+  } catch (error) {
+    res.status(500).send("Error updating todo");
+  }
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    await db.Todo.destroy({
+      where: { id: req.params.id },
+    });
+    res.json({ message: 'Todo deleted' });
+  } catch (error) {
+    res.status(500).send("Error deleting todo");
+  }
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
