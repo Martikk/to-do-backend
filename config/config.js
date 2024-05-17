336486eddff892b26a861037776bc9b474531c8f
your_name <alex.martishyn@gmail.com>
2024-05-16 21:06:46 -0700

    start

--- config/config.js
+++ config/config.js
@@ -6,20 +6,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: 'database_test',
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: 'database_production',
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   }
 };
--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ const sequelizeConfig = config[env];
 const sequelize = new Sequelize(sequelizeConfig.database, sequelizeConfig.username, sequelizeConfig.password, {
   host: sequelizeConfig.host,
   dialect: sequelizeConfig.dialect,
+  port: sequelizeConfig.port,
 });
 
 // Test the connection
